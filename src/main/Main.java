99bc59d98ac7fe4dee656b71e8585763808c0787
EnueLLL1 <Eg121745@gmail.com>
2025-10-25 15:49:39 -0300

    ADD

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 
+//SOS
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
